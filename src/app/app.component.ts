e12d456aa4a5205d8853f7c1ecfa7ce0971fc981
Rahul Patwal <patwalrahul3@gmail.com>
2024-04-03 22:10:59 +0530

    host element in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Course } from './models/course.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  courses: Course[] = [
+    {title: 'Angular Complete Cours', isActive: false},
+    {title: 'Angular Unit Testing', isActive: false},
+    {title: 'Angular RXJS', isActive: false},
+  ]
+
   title = 'ng-style-animation';
 }
